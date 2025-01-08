a672fcc11dd39f38016b46ed4769d4127d69270c
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-01-08 21:56:06 +0800

    Update_作業完成

---
+++ Assets/Scripts/Practise_1_Variable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Practise_1_Variable : MonoBehaviour
+{
+    public int price = 999;           // 價格
+    private float jump = 77.5f;       // 跳躍高度
+    public string weapon = "三項之力"; // 武器
+    private bool dragonSoul = true;  // 是否獲得龍魂
+}
