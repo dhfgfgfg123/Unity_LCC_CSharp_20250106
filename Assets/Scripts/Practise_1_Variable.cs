b28a7362425afbafdaf9b1601a132061ac060915
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-01-13 19:12:56 +0800

    Update_作業

--- Assets/Scripts/Practise_1_Variable.cs
+++ Assets/Scripts/Practise_1_Variable.cs
@@ -2,8 +2,8 @@
 
 public class Practise_1_Variable : MonoBehaviour
 {
-    public int price = 999;           // 價格
-    private float jump = 77.5f;       // 跳躍高度
-    public string weapon = "三項之力"; // 武器
-    private bool dragonSoul = true;  // 是否獲得龍魂
+    public int price = 999;           
+    private float jumpHeight = 77.5f;       
+    public string weapon = "三項之力"; 
+    private bool getDragonSoul = true;  
 }
