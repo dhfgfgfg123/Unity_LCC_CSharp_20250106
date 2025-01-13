e46db2e161a1ee66d3bdb01fad3bd5a812a4a717
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-01-13 21:30:03 +0800

    Update 認識運算子

--- Assets/Scripts/Practise_1_Variable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Practise_1_Variable : MonoBehaviour
-{
-    public int price = 999;           
-    private float jumpHeight = 77.5f;       
-    public string weapon = "三項之力"; 
-    private bool getDragonSoul = true;  
-}
