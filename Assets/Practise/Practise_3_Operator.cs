94b943c25338427f874cedf9f8c8ab5eb1f20b97
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-01-15 23:53:26 +0800

    Update_作業

---
+++ Assets/Practise/Practise_3_Operator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Practise_3_Operator : MonoBehaviour
+{
+    private void Awake()
+    {
+        Count();
+    }
+
+    private void Count()
+    {
+        Debug.Log($"<color=#f33>{6 / 2 * (1 + 2)}</color>");
+    }
+}
+
---
+++ Assets/Practise/Practise_4_Function.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Practise_4_Function : MonoBehaviour
+{
+    private void Awake()
+    {
+        True();
+        False();
+    }
+
+    private void True()
+    {
+        Debug.Log($"<color=#f33>{1 == 1}</color>");
+        
+    }
+
+    private void False()
+    {
+        Debug.Log($"<color=#f33>{1 != 1}</color>");
+        
+
+    }
+}
