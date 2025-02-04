e8991266ee6d78aebddf2b040d673d559b3e97c3
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-02-04 14:33:04 +0800

    Update HomeWork

---
+++ Assets/Practise/Practise_5_Selection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Eason
+{
+
+    public class Practise_5_Selection : MonoBehaviour
+    {
+        [SerializeField, Header("血量"), Range(0, 100)]
+        public int hp = 100;
+
+
+        private void Update()
+        {
+            switch (hp)
+            {
+                case >= 80:
+                    Debug.Log("<color=#ff3>血量安全</color>");
+                    break;
+                case >= 60:
+                    Debug.Log("<color=#ff3>健康狀態有狀況</color>");
+                    break;
+                case >= 40:
+                    Debug.Log("<color=#ff3>警告，快喝水</color>");
+                    break;
+                case >= 10:
+                    Debug.Log("<color=#ff3>快死掉了</color>");
+                    break;
+                case  0:
+                    Debug.Log("<color=#ff3>你已經死了</color>");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Assets/Practise/Practise_6_Array.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Eason
+{
+
+    public class Practise_6_Array : MonoBehaviour
+    {
+
+        public string[,,] shop =
+    {
+        //第一頁
+        { {"皮卡丘","妙蛙種子","卡比獸"} },
+        //第二頁
+        { {"藍寶","菇菇寶貝","緞帶肥肥"} },
+        //第三頁
+        { {"艾希","蓋倫","凱特林"} }
+
+      };
+
+        private void Start()
+        {
+            Debug.Log($"<color=#f33>{shop[0, 0, 0]}</color>");
+            Debug.Log($"<color=#f33>{shop[1, 0, 1]}</color>");
+            Debug.Log($"<color=#f33>{shop[2, 0, 1]}</color>");
+
+            Debug.Log($"<color=#f33>{shop[1, 0, 2]}</color>");
+        }
+    }
+}
