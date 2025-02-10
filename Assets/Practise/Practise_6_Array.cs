d9e1515ca99a09fb728dd916f51d46ab7f9d2815
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-02-10 19:08:05 +0800

    Update 作業

--- Assets/Practise/Practise_6_Array.cs
+++ Assets/Practise/Practise_6_Array.cs
@@ -6,24 +6,24 @@ namespace Eason
     public class Practise_6_Array : MonoBehaviour
     {
 
-        public string[,,] shop =
+        public string[,,] charaterName =
     {
         //第一頁
-        { {"皮卡丘","妙蛙種子","卡比獸"} },
+        { {"皮卡丘","妙蛙種子","卡比獸"},{"雷丘","噴火龍","水箭龜" } },
         //第二頁
-        { {"藍寶","菇菇寶貝","緞帶肥肥"} },
+        { {"藍寶","菇菇寶貝","緞帶肥肥"}, {"木妖","斧木妖","黑木妖" } },
         //第三頁
-        { {"艾希","蓋倫","凱特林"} }
+        { {"艾希","蓋倫","凱特林"},{"維加","漢默丁格","薩科" } }
 
       };
 
         private void Start()
         {
-            Debug.Log($"<color=#f33>{shop[0, 0, 0]}</color>");
-            Debug.Log($"<color=#f33>{shop[1, 0, 1]}</color>");
-            Debug.Log($"<color=#f33>{shop[2, 0, 1]}</color>");
+            Debug.Log($"<color=#f33>{charaterName[0, 0, 0]}</color>");
+            Debug.Log($"<color=#f33>{charaterName[1, 0, 1]}</color>");
+            Debug.Log($"<color=#f33>{charaterName[2, 0, 1]}</color>");
 
-            Debug.Log($"<color=#f33>{shop[1, 0, 2]}</color>");
+            Debug.Log($"<color=#f33>{charaterName[1, 0, 2]}</color>");
         }
     }
 }
