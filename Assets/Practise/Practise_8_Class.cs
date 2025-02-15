a47f0df7e68e3cbf6de4f0d48e34338bf329fa96
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-02-15 20:51:49 +0800

    Update 作業

---
+++ Assets/Practise/Practise_8_Class.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.NetworkInformation;
+using UnityEngine;
+
+namespace Eason
+{
+
+    public class Practise_8_Class : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Practise_8_Boss goblinKing = new Practise_8_Boss("哥布林王", "哥布林斬擊", 1000);
+            Practise_8_Boss skeletonKing = new Practise_8_Boss("骷髏王", "大腿骨衝擊", 1500);
+
+            goblinKing.Data();
+            skeletonKing.Data();
+        }
+    }
+}
