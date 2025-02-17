2123d8870577b091843480a50a542c08ea791018
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-02-17 18:57:54 +0800

    Update 作業

--- Assets/Practise/Practise_8_Class.cs
+++ Assets/Practise/Practise_8_Class.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.NetworkInformation;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Eason
 {
@@ -13,6 +12,25 @@ namespace Eason
 
             goblinKing.Data();
             skeletonKing.Data();
+        }
+    }
+
+    public class Practice_8_Boss
+    {
+        private string name;
+        private string skill;
+        private float hp;
+
+        public Practice_8_Boss(string _name, string _skill, float _hp)
+        {
+            name = _name;
+            skill = _skill;
+            hp = _hp;
+        }
+
+        public void Data()
+        {
+            Debug.Log($"<color=#3f3>名稱 : {name}, 大絕招名稱 : {skill}, 血量 : {hp}");
         }
     }
 }
