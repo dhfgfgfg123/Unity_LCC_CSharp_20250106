b736a03e06a7ea0eca09db85754954bb92ad5490
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-02-19 20:44:01 +0800

    Update 繼承

--- Assets/Scripts/Class_10_1_Inherit.cs
+++ Assets/Scripts/Class_10_1_Inherit.cs
@@ -14,6 +14,20 @@ namespace Eason
             LogSystem.LogWithColor($"哥布林1號的攻擊 : {goblin1.attack}", "#f3d");
             var slime1 = new Slime(3, 2);
             LogSystem.LogWithColor($"史萊姆1號的攻擊 : {slime1.attack}", "#f3d");
+            goblin1.Move();
+            slime1.Move();
+            slime1.Initlize();
+
+            // 判斷史萊姆是否繼承哥布林
+            // 類別A is 類別B : 布林值
+            var slime1IsGoblin = slime1 is Goblin;
+            LogSystem.LogWithColor($"史萊姆1號是否為哥布林:{slime1IsGoblin}", "#f33");
+
+            var spider1 = new Spider(15, 3);
+            var spider1IsSlime = spider1 is Slime;
+            var spider1IsGoblin = spider1 is Goblin;
+            LogSystem.LogWithColor($"蜘蛛1號是否為史萊姆:{spider1IsSlime}", "#f33");
+            LogSystem.LogWithColor($"蜘蛛1號是否為哥布林:{spider1IsGoblin}", "#f33");
         }
     }
 
@@ -23,11 +37,18 @@ namespace Eason
         public int defense;       
         private float moveSpeed;  // 私人 : 此類別可以存取
         protected float hp;       // 保護 : 子類別可以存取
+        protected float lv = 5;
 
         public Goblin(int _attack, int _defense)
         {
             attack = _attack;
             defense = _defense;
+        }
+
+        // virtural 虛擬 : 允許子類別覆寫
+        public virtual void Move()
+        {
+            LogSystem.LogWithColor("兩條腿移動", "#9f9");
         }
     }
 
@@ -36,6 +57,8 @@ namespace Eason
     // C# 僅提供單一繼承 (只能繼承一個類別)
     public class Slime : Goblin
     {
+        protected int lv = 10;
+
         public Slime(int _attack, int _defense) : base(_attack, _defense)
         {
         }
@@ -45,6 +68,25 @@ namespace Eason
             attack = 7;          // 公開 : 可以存取
             // moveSpeed = 3.5f; // 私人 : 無法存取
             hp = 10;             // 保護 : 可以存取
+            LogSystem.LogWithColor($"等級 : {lv}", "#78f");
+            LogSystem.LogWithColor($"等級 : {base.lv} (父類別)", "#78f");
+        }
+
+        // override 覆寫 : 覆寫父類別有 virtual 關鍵字的成員
+        // override 選擇要覆寫的成員 > 按下 Enter 自動完成
+        public override void Move()
+        {
+            // base.Move();   // 父類別原有的內容
+            LogSystem.LogWithColor("爬行", "#3f3");
+        }
+
+        
+    }
+
+    public class Spider : Slime
+    {
+        public Spider(int _attack, int _defense) : base(_attack, _defense)
+        {
         }
     }
 }
