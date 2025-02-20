1b231d88abc5c63c08c13c42d3fd4665a5cf7347
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-02-20 23:53:31 +0800

    Update 作業

---
+++ Assets/Practise/Practise_10.cs
@@ -0,0 +1,70 @@
+﻿using Eason.Tools;
+using UnityEngine;
+
+namespace Eason
+{
+
+    public class Practise_10 : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Item potion1 = new Item("紅水");
+            Potion potion2 = new Potion("藍水", 100);
+            Equipment equipment1 = new Equipment("頭盔", 10);
+
+            potion1.Use();
+            potion2.Use();
+            equipment1.Use();
+        }
+    }
+
+    public class Item
+    {
+        public string name;
+        
+        public Item() { }
+        public Item(string _name)
+        {
+            name = _name;
+        }
+        public virtual void Use()
+        {
+            LogSystem.LogWithColor($"使用 : {name}", "#f33");
+        }
+
+    }
+
+    public class Potion : Item
+    {
+        public string name;
+        public float heal;
+
+        public Potion(string _name, float _heal)
+        {
+            name = _name;
+            heal = _heal;
+        }
+
+        public void Use()
+        {
+            LogSystem.LogWithColor($"使用 : {name} 補充 : {heal} 點魔力", "#f33");
+        }
+    }
+
+    public class Equipment : Item
+    {
+        public string name;
+        public float weight;
+
+        public Equipment(string _name, float _weight)
+        {
+            name = _name;
+            weight = _weight;
+        }
+
+        public override void Use()
+        {
+            LogSystem.LogWithColor($"裝備 : {name} 需承受 : {weight} 重量", "#f33");
+        }
+    }
+}
---
+++ Assets/Practise/Practise_9.cs
@@ -0,0 +1,19 @@
+﻿using Eason.Tools;
+using System;
+using UnityEngine;
+
+namespace Eason
+{
+
+    public class Practise_9 : MonoBehaviour
+    {
+
+        private void Awake()
+        {
+            decimal count = -999.321m;
+            SByte decimalToSbyte = Convert.ToSByte(count);
+            LogSystem.LogWithColor(decimalToSbyte, "#f33");
+            LogSystem.LogWithColor(decimalToSbyte.GetType(), "#f33");
+        }
+    }
+}
