f757bede92b6a01b5924077aa3ca26b6776b02fa
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-03-10 21:39:20 +0800

    Update 泛型

--- Assets/Scripts/Class_15_Generics.cs
+++ Assets/Scripts/Class_15_Generics.cs
@@ -10,6 +10,7 @@ namespace Eason.Class_15
     {
         private void Awake()
         {
+            #region 不使用泛型
             int numberA = 7, numberB = 9;
             LogSystem.LogWithColor($"數字 A 與 B : {numberA} | {numberB}", "#f93");
             SwapNumber(ref numberA, ref numberB);
@@ -18,21 +19,41 @@ namespace Eason.Class_15
             char charA = '嗨', charB = '嘿';
             LogSystem.LogWithColor($"字元 A 與 B : {charA} | {charB}", "#f93");
             SwapChar(ref charA, ref charB);
-            LogSystem.LogWithColor($"字元 A 與 B : {charA} | {charB}", "#F93");
+            LogSystem.LogWithColor($"字元 A 與 B : {charA} | {charB}", "#f93");
 
             object objA = 3.5f, objB = 7.7f;
             LogSystem.LogWithColor($"物件 A 與 B : {objA} | {objB}", "#f93");
             SwapObject(ref objA, ref objB);
-            LogSystem.LogWithColor($"物件 A 與 B : {objA}  |  {objB}", "#F93");
+            LogSystem.LogWithColor($"物件 A 與 B : {objA}  |  {objB}", "#f93");
+            #endregion
+
+            bool boolA = true, boolB = false;
+            LogSystem.LogWithColor($" A 與 B : {boolA} | {boolB}", "#f93");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSystem.LogWithColor($" A 與 B : {boolA} | {boolB}", "#F93");
+
+            byte byteA = 1, byteB = 9;
+            LogSystem.LogWithColor($" A 與 B : {byteA} | {byteB}", "#f93");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSystem.LogWithColor($" A 與 B : {byteA} | {byteB}", "#F93");
+
+            var player1 = new DataPlayer<int>();
+            player1.data = 99;
+            player1.LogData(123);
+
+            var player2 = new DataPlayer<string>();
+            player2.data = "玩家二號";
+            player2.LogData("哈囉");
         }
 
 
+        #region 方法區域
         /// <summary>
         /// 數字對調
         /// </summary>
         /// <param name="a">第一個數字</param>
         /// <param name="b">第二個數字</param>
-        public void SwapNumber(ref int a,ref int b)
+        public void SwapNumber(ref int a, ref int b)
         {
             int temp = a;
             a = b;
@@ -51,11 +72,40 @@ namespace Eason.Class_15
             b = temp;
         }
 
+        /// <summary>
+        /// 物件對調
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
         public void SwapObject(ref object a, ref object b)
         {
             object temp = a;
             a = b;
             b = temp;
+        }
+
+        /// <summary>
+        /// 使用泛型進行對調
+        /// </summary>
+        /// <typeparam name="T">要對調的資料類型</typeparam>
+        /// <param name="a">第一個資料</param>
+        /// <param name="b">第二個資料</param>
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        } 
+        #endregion
+
+        public class DataPlayer<T>
+        {
+            public T data;
+
+            public void LogData(T data)
+            {
+                LogSystem.LogWithColor(data, "#3ff");
+            }
         }
     }
 }
