819a51f6f5afa82daec445f93aad197b69767efe
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-03-17 20:51:39 +0800

    Update 認識字典與排序字典

--- Assets/Scripts/Class_16_DataStruct.cs
+++ Assets/Scripts/Class_16_DataStruct.cs
@@ -17,7 +17,7 @@ namespace Eason.Class_16
         // 清單 : 泛型集合
         public List<int> speeds = new List<int>() { 3, 9, 7 };
         public List<string> props = new List<string>() { "藥水", "地圖" };
-        public List<float> playerDefens; 
+        public List<float> playerDefens;
         #endregion
 
         private void Awake()
@@ -84,6 +84,7 @@ namespace Eason.Class_16
             LogStack<string>(enemys);
             #endregion
 
+            #region 佇列
             // Queue 佇列 : 先進先出，先放進來的資料先被拿出來
             Queue<string> player = new Queue<string>();
             player.Enqueue("盜賊");
@@ -96,8 +97,81 @@ namespace Eason.Class_16
             // 拿東西並刪除，與堆疊 Pop 相同
             LogSystem.LogWithColor(player.Dequeue(), "#f33");
             LogQueue<string>(player);
-            
-        } 
+            #endregion
+
+            #region 鏈結串列
+            // LinkekList 鏈結串列
+            string[] skillsArray = new string[] { "火球", "冰錐" };
+            LinkedList<string> skills = new LinkedList<string>(skillsArray);
+            LogLinkedList<string>(skills);
+            skills.AddLast("落雷");
+            LogLinkedList<string>(skills);
+            skills.AddFirst("岩石");
+            LogLinkedList<string>(skills);
+
+            // 在火球後面添加一個毒霧
+            LinkedListNode<string> skillFire = skills.Find("火球");
+            skills.AddAfter(skillFire, "毒霧");
+            // 再火球前添加一個瞬移
+            skills.AddBefore(skillFire, "瞬移");
+            LogLinkedList<string>(skills);
+            #endregion
+
+            #region 排序集合
+            SortedSet<int> counts = new SortedSet<int> { 9, 2, 80, 1 };
+            LogSortedSet<int>(counts);
+            counts.Add(77);
+            counts.Add(123);
+            counts.Add(9);
+            LogSortedSet<int>(counts);
+            LogSystem.LogWithColor($"最大 : {counts.Max}", "#f33");
+            LogSystem.LogWithColor($"最小 : {counts.Min}", "#f33");
+
+            SortedSet<int> lv = new SortedSet<int> { 7, 3, 78, 123, 5, 80 };
+            // 交集與差集
+            counts.IntersectWith(lv);
+            LogSortedSet<int>(counts);
+            counts.ExceptWith(lv);
+            LogSortedSet<int>(counts);
+            #endregion
+
+            #region 字典
+            Dictionary<int, string> deck = new Dictionary<int, string>()
+            {
+                {10, "真紅眼黑龍" }, {3, "落穴"}, {1, "黑魔導"}
+            };
+            LogDictionary<int, string>(deck);
+            deck.Add(7, "死者甦醒");
+            LogSystem.LogWithColor($"是否有編號 3 資料 : {deck.ContainsKey(3)}", "#f33");
+            LogSystem.LogWithColor($"是否有羽毛掃資料 : {deck.ContainsValue("羽毛掃")}", "#f33"); 
+            #endregion
+
+            // 保持排序並且不會有重複的鍵
+            SortedList<string, int> board = new SortedList<string, int>();
+            board.Add("Eason", 60);
+            board.Add("Kevin", 85);
+            board.Add("Cherry", 85);
+            //board.Add("Cherry", 77);               // 重複的鍵導致錯誤
+            LogSortedList<string, int>(board);
+
+            // 保持排序的字典
+            SortedDictionary<string, int> scores = new SortedDictionary<string, int>();
+            scores.Add("Eason", 90);
+            scores.Add("Kevin", 85);
+            scores.Add("Cherry", 85);
+            // scores.Add("Cherry", 70);             // 重複的鍵導致錯誤
+            LogSortedDictionary<string, int>(scores);
+
+            // SortedList 與 SortedDictionary 的差異
+            // 1.SortedList 是使用陣列方式儲存，比較省記憶體空間
+            // 2.SortedDictionart 是使用樹狀結構方式儲存，比較占記憶體空間
+            // 3.SortedList 可以使用索引值存取 [0]
+            LogSystem.LogWithColor($"排行榜第一筆 : {board.Keys[0]}", "#f93");
+            // LogSystem.LogWithColor($"排行榜第一筆 : {scores.Keys[0]})", "#f93");
+            // 4. SortedList 大量資料增減時比較占記憶體
+            // 如果資料不需要頻繁的增減建議使用 SortedList 否之建議使用 SortedDictionary
+
+        }
 
         private void LogStack<T>(Stack<T> stack)
         {
@@ -115,6 +189,54 @@ namespace Eason.Class_16
             {
                 LogSystem.LogWithColor($"佇列資料 : {item}", "#7f7");
             }
+        }
+
+        private void LogLinkedList<T>(LinkedList<T> linkedList)
+        {
+            foreach (var item in linkedList)
+            {
+                LogSystem.LogWithColor(item, "#fa3");
+            }
+
+            LogSystem.LogWithColor("----------", "#fff");
+        }
+
+        private void LogSortedSet<T>(SortedSet<T> set)
+        {
+            foreach (var item in set)
+            {
+                LogSystem.LogWithColor(item, "#3f3");
+            }
+
+                LogSystem.LogWithColor("----------", "#fff");
+        }
+
+        private void LogDictionary<T, U>(Dictionary<T, U> dict)
+        {
+            foreach (var item in dict)
+            {
+                LogSystem.LogWithColor($"卡牌的編號 : {item.Key}", "#3f3");
+                LogSystem.LogWithColor($"卡牌的名稱 : {item.Value}", "#3f3");
+            }
+            LogSystem.LogWithColor("----------", "#fff");
+        }
+
+        private void LogSortedList<T, U>(SortedList<T, U> list)
+        {
+            foreach (var item in list)
+            {
+                LogSystem.LogWithColor($"{item.Key} 的分數 {item.Value}", "#77f");
+            }
+            LogSystem.LogWithColor("----------", "#fff");
+        }
+
+        private void LogSortedDictionary<T, U>(SortedDictionary<T, U> dict)
+        {
+            foreach (var item in dict)
+            {
+                LogSystem.LogWithColor($"{item.Key} 的分數 {item.Value}", "#77f");
+            }
+            LogSystem.LogWithColor("----------", "#fff");
         }
     }
 }
