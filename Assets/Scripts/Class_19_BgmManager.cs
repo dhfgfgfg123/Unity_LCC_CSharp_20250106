9152c510a1a6b194d2a9fc469700fa02bda58aaf
dhfgfgfg123 <165664087+dhfgfgfg123@users.noreply.github.com>
2025-03-24 20:48:46 +0800

    Update 認識事件

---
+++ Assets/Scripts/Class_19_BgmManager.cs
@@ -0,0 +1,34 @@
+﻿using Eason.Tools;
+using System;
+using UnityEngine;
+
+namespace Eason.Class_19.Event
+{
+    /// <summary>
+    /// 背景音樂管理器
+    /// </summary>
+    public class Class_19_BgmManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Class_19_Event class_19_event;
+
+        private void Awake()
+        {
+            // 步驟 3. 其他系統訂閱事件
+            class_19_event.onDead += ChangeBgm;
+            class_19_event.onDeadAction += ChangeBgmViaHp;
+        }
+
+        private void ChangeBgmViaHp(string arg1, float arg2)
+        {
+            LogSystem.LogWithColor($"{arg1}, | 的血量 : {arg2}", "#78f");
+            if (arg2 <= -5) LogSystem.LogWithColor("變更為遊戲結束音樂", "#f777");
+        }
+
+        private void ChangeBgm()
+        {
+            LogSystem.LogWithColor("變更音樂", "#3f3");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Class_19_Event.cs
@@ -0,0 +1,55 @@
+﻿using Eason.Tools;
+using System;
+using UnityEngine;
+
+namespace Eason.Class_19.Event
+{
+    /// <summary>
+    /// 事件 : 當一件事情發生後會通知所有系統
+    /// 需要對應處理的系統可以訂閱並做出處理
+    /// 
+    /// 遊戲範例 : 死亡事件
+    /// GM 死亡事件，當角色會執行死亡事件並通知其他系統
+    /// 控制系統 > 訂閱死亡事件 >停止控制系統
+    /// 音樂系統 > 訂閱死亡事件 > 變更 BGM
+    /// </summary>
+    public class Class_19_Event : MonoBehaviour
+    {
+        // 事件使用步驟
+        // 1. 宣告事件 (委派)
+        // 2. 呼叫事件(什麼時間點要觸發)
+        // 3. 其他系統訂閱事件
+
+        [SerializeField,Header("血量")]
+        private float hp = 10;
+
+        public delegate void delegateMethod();
+        // 事件關鍵字 : event
+        public  event delegateMethod onDead;
+
+        public event Action<string, float> onDeadAction;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) Damage(5);
+
+        }
+
+        private void Damage(float damage)
+        {
+            hp -= damage;
+
+            if (hp <= 0)
+            {
+                LogSystem.LogWithColor("死亡", "#f99");
+                // 步驟 2. 呼叫事件
+                onDead?.Invoke();                         // onDead 如果有訂閱者才會呼叫
+                //onDead();                               // onDead 如果沒有訂閱者會報錯
+
+                onDeadAction?.Invoke("玩家", hp);         // 事件有參數時呼叫要帶入
+            }
+        }
+
+        
+    }
+}
